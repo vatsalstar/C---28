6ae983aad711c45a6e91c350b4d43752b0b93aee
vatsalstar <74692621+vatsalstar@users.noreply.github.com>
2021-01-22 08:06:07 +0530

    Add files via upload

--- Mango.js
+++ Mango.js
@@ -1,31 +1,20 @@
-class Mango extends BaseClass {
-  constructor(x, y){
-    super(x,y,50,50);
-    this.image = loadImage("sprites/mango.png");
-    this.Visiblity = 255;
-  }
-
- display(){
-   //console.log(this.body.speed);
-   if(this.body.speed < 3){
-    super.display();
-   }
-   else{
-     World.remove(world, this.body);
-     push();
-     this.Visiblity = this.Visiblity - 5;
-     tint(255,this.Visiblity);
-     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
-     pop();
-   }
-  }
-
-  score(){
-    if (this.Visiblity < 0 && this.Visiblity > -1005){
-      score++;
+class Mango { 
+  constructor(x,y,radius){ 
+    var opt = { isStatic : true, friction : 1, restitution : 0 } 
+    this.body = Bodies.circle(x,y,radius,opt);
+     this.radius = radius; 
+     this.image = loadImage("Images/mango.png");
+      World.add(world,this.body);
+     }
+     
+     display(){ 
+       var angle = this.body.angle;
+       push();
+        translate(this.body.position.x, this.body.position.y);
+         rotate(angle); 
+  imageMode(CENTER);
+   image(this.image, 0, 0, this.radius*6.5, this.radius*6.5);
+    pop(); 
+    
+     } 
     }
-  }
-
-
-
-};
--- Stone.js
+++ Stone.js
@@ -1,48 +1,12 @@
-class Stone{
-    constructor(bodyA, pointB){
-        var options = {
-            bodyA: bodyA,
-            pointB: pointB,
-            stiffness: 0.04,
-            length: 10
+class Stone 
+{ constructor(x,y)
+    {var opt = { density : 1.2 } 
+    this.body = Bodies.circle(x,y,40,opt);
+     World.add(world,this.body) 
+     this.image = loadImage("Images/stone.png");
+     } 
+     display(){ 
+         imageMode(CENTER); 
+         image(this.image,this.body.position.x,this.body.position.y,40,40);
+         } 
         }
-        this.stone = loadImage('sprites/stone.png');       
-        this.pointB = pointB
-        this.stone = Constraint.create(options);
-        World.add(world, this.stone);
-    }
-    attach(body){
-        this.stone.bodyA = body;
-    }
-    
-    fly(){
-        this.stone.bodyA = null;
-    }
-
-    display(){
-        image(this.stone,200,20);
-        if(this.stone.bodyA){
-            var pointA = this.stone.bodyA.position;
-            var pointB = this.pointB;
-            push();
-            
-            stroke(48,22,8);
-            if(pointA.x < 220) {
-                strokeWeight(7);
-                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
-                image(this.sling3,pointA.x -30, pointA.y -10,15,30);
-            }
-            else{
-                strokeWeight(3);
-                line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
-                line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
-                image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
-            }
-           
-            
-            pop();
-        }
-    }
-    
-}
